86ff9a74066a84ad31c599f0f493b4a0998efe78
Bobby Holley <bobbyholley@gmail.com>
2011-03-27 11:26:19 -0700

    Imported holdingPage.{js,html}

---
+++ data/holdingPage.js
@@ -0,0 +1,40 @@
+// holdingPage.html script file
+
+// Globals
+var secondsLeft = 5;
+
+// Updates the counter display
+function updateCounterDisplay() {
+    document.getElementById("timeLeft").innerHTML = secondsLeft;
+}
+
+// Set anothe timeout
+function armTimeout() {
+    window.setTimeout(tick, 1000);
+}
+
+// Called every second
+function tick() {
+
+    // Decrement the time left
+    secondsLeft--;
+    
+    // Update the page
+    updateCounterDisplay();
+    
+    // Forward if we've waited long enough
+    if (secondsLeft == 0) {
+        var baseURI = decodeURIComponent(getURLParam(window.location.href, "dst"));
+        var fullURI = addURLParam(baseURI, "LetGoWaitComplete", "true");
+        window.location.replace(fullURI);
+    }
+    // Otherwise, reset the timer
+    else
+        armTimeout();
+}
+
+// Init code
+window.onload = function() {
+  updateCounterDisplay();
+  armTimeout();
+};
