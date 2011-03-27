1eb52df303ef183687a9cb4fc66595d522db1148
Bobby Holley <bobbyholley@gmail.com>
2011-03-27 11:36:01 -0700

    Retabbed and commented.

--- data/holdingPage.js
+++ data/holdingPage.js
@@ -5,32 +5,32 @@ var secondsLeft = 5;
 
 // Updates the counter display
 function updateCounterDisplay() {
-    document.getElementById("timeLeft").innerHTML = secondsLeft;
+  document.getElementById("timeLeft").innerHTML = secondsLeft;
 }
 
 // Set anothe timeout
 function armTimeout() {
-    window.setTimeout(tick, 1000);
+  window.setTimeout(tick, 1000);
 }
 
 // Called every second
 function tick() {
 
-    // Decrement the time left
-    secondsLeft--;
-    
-    // Update the page
-    updateCounterDisplay();
-    
-    // Forward if we've waited long enough
-    if (secondsLeft == 0) {
-        var baseURI = decodeURIComponent(getURLParam(window.location.href, "dst"));
-        var fullURI = addURLParam(baseURI, "LetGoWaitComplete", "true");
-        window.location.replace(fullURI);
-    }
-    // Otherwise, reset the timer
-    else
-        armTimeout();
+  // Decrement the time left
+  secondsLeft--;
+
+  // Update the page
+  updateCounterDisplay();
+
+  // Forward if we've waited long enough
+  if (secondsLeft == 0) {
+    var baseURI = decodeURIComponent(getURLParam(window.location.href, "dst"));
+    var fullURI = addURLParam(baseURI, "LetGoWaitComplete", "true");
+    window.location.replace(fullURI);
+  }
+  // Otherwise, reset the timer
+  else
+    armTimeout();
 }
 
 // Init code
--- data/workerScript.js
+++ data/workerScript.js
@@ -3,20 +3,20 @@
 // We can't act until we get some important information from the add-on.
 onMessage = function(message) {
 
-    // If this tab is unguarded, we let it go.
-    if (message.unguarded)
-        return;
+  // If this tab is unguarded, we let it go.
+  if (message.unguarded)
+    return;
 
-    // If we were bounced back by the holding page, the user has waited enough.
-    if (getURLParam(window.location.href, "LetGoWaitComplete") === "true") {
+  // If we were bounced back by the holding page, the user has waited enough.
+  if (getURLParam(window.location.href, "LetGoWaitComplete") === "true") {
 
-        // Post a message to the addon telling it to unguard this tab
-        postMessage({});
+    // Post a message to the addon telling it to unguard this tab
+    postMessage({});
+  }
 
-    }
-
-    // Otherwise, we need to bounce to the holding page
-    else {
-        window.location.replace(message.holdingPageURL + '?' + 'dst=' + encodeURIComponent(window.location.href));
-    }
+  // Otherwise, we need to bounce to the holding page
+  else {
+    window.location.replace(message.holdingPageURL + '?' + 'dst=' +
+                            encodeURIComponent(window.location.href));
+  }
 }
